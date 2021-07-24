b6894e13c787992d19b3a38783b96fb80772977c
dell <hans040515@gmail.com>
2021-07-24 23:19:29 +0800

    can upload files

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request){
@@ -17,6 +19,28 @@ func UploadHandler(w http.ResponseWriter, r *http.Request){
 		io.WriteString(w, string(data))
 	}else if r.Method == "POST"{
 		//receive files and save
+		file,head,err := r.FormFile("file")
+		if err!=nil{
+			fmt.Printf("Failed to get data, error:%s\n", err.Error())
+			return
+		}
+		defer file.Close()
+
+		//create a new file, later will be used for copy
+		newFile,err:=os.Create("./tmp/"+head.Filename)
+		if err!=nil{
+			fmt.Printf("Failed to create file, error:%s\n", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		_, err = io.Copy(newFile, file)
+		if err!=nil{
+			fmt.Printf("Failed to save data to file, err:%s\n", err.Error())
+			return
+		}
 	}
+
+
 
 }
